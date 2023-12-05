f69fd6a9f57cd5d62c37cac860e226afce4edde3
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2023-12-05 17:07:24 +0100

    fix

--- sae.sh
+++ sae.sh
@@ -10,7 +10,6 @@ fi
 
 declare -A mois=( ["1"]="janvier" ["2"]="fevrier" ["3"]="mars" ["4"]="avril" ["5"]="mai" ["6"]="juin" ["7"]="juillet" ["8"]="aout" ["9"]="septembre" ["10"]="octobre" ["11"]="novembre" ["12"]="decembre")
 
-
 for line in $(cat "$1"); do
 	nom=$(echo $line | cut -d":" -f1)
 	prenom=$(echo $line | cut -d":" -f2)
@@ -76,5 +75,9 @@ for line in $(cat "$1"); do
 	password="$lettre_nom$lettre_prenom${numtel:2:1}${spec}${mois[$mois_naiss]:0:1}"
 
 	useradd -g A$annee -p $password -m -b /home/A$annee $username
+	if (( $? != 0 )); then
+		echo "Erreur lors de la création de l'utilisateur $username"
+		exit 1
+	fi
 	echo "$nom:$prenom:$username:$password" >> /root/A$annee.password
 done
