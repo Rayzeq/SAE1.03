911d7f8fc3d4750e3f3f684315edb477816d64ce
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-01-09 17:42:00 +0100

    more checks MORE CHECKS

--- create_users.sh
+++ create_users.sh
@@ -6,19 +6,23 @@ if [ "$EUID" != 0 ]; then
 fi
 
 if (( $# != 1 )); then
-	echo "Veuiller préciser un et un seul argument"
+	echo "Veuiller préciser un et un seul argument" >&2
 	exit 1
 fi
 
 if [ ! -f "$1" ]; then
-	echo "$1 n'est pas un fichier valide"
-	exit 1
+	echo "$1 n'est pas un fichier valide" >&2
+	exit 2
 fi
 
 declare -A mois=( ["1"]="janvier" ["2"]="fevrier" ["3"]="mars" ["4"]="avril" ["5"]="mai" ["6"]="juin" ["7"]="juillet" ["8"]="aout" ["9"]="septembre" ["10"]="octobre" ["11"]="novembre" ["12"]="decembre")
 
-local IFS=$'\n'
+IFS=$'\n'
 for line in $(cat "$1"); do
+	if (($(echo $line | fold -w1 | grep : | wc -l) != 4)); then
+		echo "$1 n'est pas un fichier valide (une ligne n'a pas assez de champs)" >&2
+		exit 3
+	fi
 	nom=$(echo $line | cut -d":" -f1)
 	prenom=$(echo $line | cut -d":" -f2)
 	((annee=$(echo $line | cut -d":" -f3)))
@@ -26,13 +30,24 @@ for line in $(cat "$1"); do
 	datenaiss=$(echo $line | cut -d":" -f5)
 
 	if [ -z "$nom" ] || [ -z "$prenom" ] || [ -z "$annee" ] || [ -z "$numtel" ] || [ -z "$datenaiss" ]; then
-		echo "Format de fichier invalide (des champs sont manquants)"
-		exit 1
+		echo "Format de fichier invalide (Des champs sont manquants)" >&2
+		exit 4
+	fi
+
+	if (( ${#numtel} < 10 )) then
+		echo "Format de fichier invalide (Le numéro de téléphone est pas assez long)" >&2
+		exit 5
+	fi
+
+	re='^[0-9]+$'
+	if ! [[ $numtel =~ $re ]]; then
+		echo "Format de fichier invalide (Le numéro de téléphone n'est pas valide)" >&2
+		exit 6
 	fi
 
 	if (( $annee < 1 || $annee > 3 )); then
-		echo "L'année doit être comprise en 1 et 3"
-		exit 1
+		echo "L'année doit être comprise en 1 et 3" >&2
+		exit 7
 	fi
 
 	((jour_naiss=($(echo $datenaiss | cut -d"/" -f1))))
@@ -40,17 +55,23 @@ for line in $(cat "$1"); do
 	((annee_naiss=$(echo $datenaiss | cut -d"/" -f3)))
 
 	if [ -z "$jour_naiss" ] || [ -z "$mois_naiss" ] || [ -z "$annee_naiss" ]; then
-		echo "Date invalide"
-		exit 1
+		echo "Date invalide" >&2
+		exit 8
+	fi
+
+	if ((( $annee_naiss % 4 != 0 )) && (( $annee_naiss % 400 != 0 ))) && ((( $mois_naiss == 2 )) && (( $jour_naiss >= 29 ))); then
+		echo "Date invalide" >&2
+		exit 9
 	fi
 
 	if (( $jour_naiss < 1 || $jour_naiss > 31 )); then
-		echo "Format de fichier invalide"
-		exit 1
+		echo "Format de fichier invalide" >&2
+		exit 10
 	fi
+
 	if (( $mois_naiss < 1 || $mois_naiss > 12 )); then
-		echo "Format de fichier invalide"
-		exit 1
+		echo "Format de fichier invalide" >&2
+		exit 11
 	fi
 
 	username="$(echo ${prenom:0:1} | tr "[a-z]" "[A-Z]")_$nom"
@@ -60,11 +81,17 @@ for line in $(cat "$1"); do
 	spec=$(echo "$^%*:;.,?#~[|@]+*-\\/=)(_&)}\!" | fold -w1 | shuf -n1)
 	password="$lettre_nom$lettre_prenom${numtel:2:1}${spec}${mois[$mois_naiss]:0:1}"
 
+	cat "$1" | grep "^$username:"
+	if (( $? == 1 )); then
+		echo "L'utilisateur $username existe déjà" >&2
+		exit 12
+	fi
+
 	useradd -g "A$annee" -m -b "/home/A$annee" "$username"
-	echo "$username:$password" | chpasswd
 	if (( $? != 0 )); then
 		echo "Erreur lors de la création de l'utilisateur $username"
-		exit 1
+		exit 13
 	fi
+	echo "$username:$password" | chpasswd
 	echo "$nom:$prenom:$username:$password" >> /root/A$annee.password
 done
